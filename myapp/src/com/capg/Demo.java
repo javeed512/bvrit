2a1b43ff34326414d19e67097cbe72584f565c6e
javeed <javeedmohammedjaveed@gmail.com>
2020-02-19 21:57:12 -0800

    hh

---
+++ myapp/src/com/capg/Demo.java
@@ -0,0 +1,35 @@
+package com.capg;
+
+public class Demo {
+	
+	int x;
+
+	public static void main(String[] args) {
+
+		Demo d = new Demo();
+		
+		System.out.println(d);
+		
+		// TODO Auto-generated method stub
+
+		d.m1();
+		
+	}
+
+	
+	private boolean m1() {
+		
+		
+		
+		if(5>3) {
+			
+			return true;
+			
+			
+		}
+	
+		return false;
+	}
+	
+	
+}
