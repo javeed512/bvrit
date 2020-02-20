4f5002069ec2f976a37dbdcf354c07e86be77579
javeed <javeedmohammedjaveed@gmail.com>
2020-02-19 22:58:11 -0800

    gf

---
+++ healthcare/src/main/java/com/capg/healthcare/HealthApp.java
@@ -0,0 +1,15 @@
+package com.capg.healthcare;
+
+public class HealthApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			System.out.print("Hello Welcome to Health Care**");
+		
+		
+		
+	}
+
+}
