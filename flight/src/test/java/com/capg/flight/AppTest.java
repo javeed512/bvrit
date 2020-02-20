5b7a02347213fd4da35fab2da802fbf4015a555b
javeed <javeedmohammedjaveed@gmail.com>
2020-02-19 23:45:42 -0800

    flight

---
+++ flight/src/test/java/com/capg/flight/AppTest.java
@@ -0,0 +1,36 @@
+package com.capg.flight;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+	@Test
+	public void test1() {
+		
+		assertEquals(1,1);
+		
+	}
+
+	
+	@Test
+	public void test2() {
+		
+		assertEquals(1,1);
+		
+	}
+
+	@Test
+	public void test3() {
+		
+		assertEquals(1,1);
+		
+	}
+
+
+
+}
